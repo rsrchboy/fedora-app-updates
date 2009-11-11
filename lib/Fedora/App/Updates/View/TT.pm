713bd72c13b74af8fd529aadd0de3f91073d0b34
Chris Weyl <cweyl@alumni.drew.edu>
2009-11-10 23:31:15 -0800

    initial add of TT view

---
+++ lib/Fedora/App/Updates/View/TT.pm
@@ -0,0 +1,41 @@
+package Fedora::App::Updates::View::TT;
+
+use strict;
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config({
+    INCLUDE_PATH => [
+        Fedora::App::Updates->path_to( 'root', 'src' ),
+        Fedora::App::Updates->path_to( 'root', 'lib' )
+    ],
+    PRE_PROCESS  => 'config/main',
+    WRAPPER      => 'site/wrapper',
+    ERROR        => 'error.tt2',
+    TIMER        => 0
+});
+
+=head1 NAME
+
+Fedora::App::Updates::View::TT - Catalyst TTSite View
+
+=head1 SYNOPSIS
+
+See L<Fedora::App::Updates>
+
+=head1 DESCRIPTION
+
+Catalyst TTSite View.
+
+=head1 AUTHOR
+
+Chris Weyl
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
+
