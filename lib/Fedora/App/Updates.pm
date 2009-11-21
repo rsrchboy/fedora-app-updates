64e331d53ce52bece97d6aa25751b81e45b5959c
Chris Weyl <cweyl@alumni.drew.edu>
2009-11-20 20:42:00 -0800

    update license verbiage

--- lib/Fedora/App/Updates.pm
+++ lib/Fedora/App/Updates.pm
@@ -56,10 +56,27 @@ L<Fedora::App::Updates::Controller::Root>, L<Catalyst>
 
 Chris Weyl
 
-=head1 LICENSE
+=head1 LICENCE AND COPYRIGHT
 
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
+Copyright (c) 2008, Chris Weyl <cweyl@alumni.drew.edu>
+
+This library is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free
+Software Foundation; either version 2.1 of the License, or (at your option)
+any later version.
+
+This library is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+OR A PARTICULAR PURPOSE.
+
+See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this library; if not, write to the
+
+    Free Software Foundation, Inc.,
+    59 Temple Place, Suite 330,
+    Boston, MA  02111-1307 USA
 
 =cut
 
--- lib/Fedora/App/Updates/Model/Updates.pm
+++ lib/Fedora/App/Updates/Model/Updates.pm
@@ -5,7 +5,7 @@ use base 'Catalyst::Model::DBIC::Schema';
 
 __PACKAGE__->config(
     schema_class => 'Fedora::App::Updates::Schema',
-    
+
     connect_info => {
         dsn      => 'dbi:mysql:mysql_ssl=1;dbname=fedora;hostname=172.16.42.70',
         user     => 'XXXXX',
@@ -33,10 +33,27 @@ Catalyst::Helper::Model::DBIC::Schema - 0.30
 
 Chris Weyl
 
-=head1 LICENSE
+=head1 LICENCE AND COPYRIGHT
 
-This library is free software, you can redistribute it and/or modify
-it under the same terms as Perl itself.
+Copyright (c) 2008, Chris Weyl <cweyl@alumni.drew.edu>
+
+This library is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free
+Software Foundation; either version 2.1 of the License, or (at your option)
+any later version.
+
+This library is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+OR A PARTICULAR PURPOSE.
+
+See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this library; if not, write to the
+
+    Free Software Foundation, Inc.,
+    59 Temple Place, Suite 330,
+    Boston, MA  02111-1307 USA
 
 =cut
 
